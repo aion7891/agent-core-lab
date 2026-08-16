57b6ff9328b14da26b91b9366cfb9aa0378666a2
Kumar Nishant <knish5790@gmail.com>
2026-08-17 03:38:19 +0530

    adding adding health checks function

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"This is a rex agent service for all types of agents !"}
+
+@app.post("/health")
+async def healthcheck():
+    return {"status": "healthy"}
